8b5eeadaefe29e1a0a8e0504098bffa38bf467b7
Ayush Bohra <codefingers809@gmail.com>
2023-04-03 20:07:32 +0530

    smart inputs

--- automator.py
+++ automator.py
@@ -2,6 +2,7 @@
 from pyautocad import Autocad, APoint, aDouble
 import math
 acad = Autocad(create_if_not_exists=True)
+pointsDB = []
 def polyline():
     inpP = [
         eval(i) for i in input("Enter \'x y\' of initial point: ").split(" ")
@@ -21,13 +22,16 @@ def polyline():
         if a == "e":
             break
 def lineL():
-    inpP1 = [eval(i) for i in input("Enter \'x y\' of point 1: ").split(" ")]
-    p1 = APoint(inpP1[0], inpP1[1])
-    inpP2 = [eval(i) for i in input("Enter \'x y\' of point 2: ").split(" ")]
-    p2 = APoint(inpP2[0], inpP2[1])
+    inpP1 = input("Enter \'x y\' of point 1: ").split(" ")
+    inpP1 = [x for x in inpP1 if x not in [None, "", [], {}, (), "undefined"]]
+    p1 = APoint(int(inpP1[0]), int(inpP1[1]))
+    inpP2 = input("Enter \'x y\' of point 2: ").split(" ")
+    inpP2 = [x for x in inpP2 if x not in [None, "", [], {}, (), "undefined"]]
+    p2 = APoint(int(inpP2[0]), int(inpP2[1]))
     l1 = acad.model.AddLine(p1, p2)
 def lineR():
-    inpPoint = [eval(i) for i in input("Enter \'x y\' of point: ").split(" ")]
+    inpPoint = input("Enter \'x y\' of point 1: ").split(" ")
+    inpPoint = [x for x in inpPoint if x not in [None, "", [], {}, (), "undefined"]]
     l = int(input("Enter length of line: "))
     a = math.radians(int(input("Enter angle of line in degrees: ")))
     point = APoint(inpPoint[0], inpPoint[1])
@@ -35,27 +39,37 @@ def lineR():
     y2 = inpPoint[0] + l * math.sin(a)
     l2 = acad.model.AddLine(point, APoint(x2, y2))
 def circle():
-    c = [eval(i) for i in input("Enter \'x y\' of center: ").split(" ")]
+    c = input("Enter \'x y\' of point 1: ").split(" ")
+    c = [x for x in c if x not in [None, "", [], {}, (), "undefined"]]
     r = int(input("Enter radius of circle: "))
     c1 = acad.model.AddCircle(APoint(c[0], c[1]), r)
 def arc():
-    c = [eval(i) for i in input("Enter \'x y\' of center: ").split(" ")]
+    c = input("Enter \'x y\' of point 1: ").split(" ")
+    c = [x for x in c if x not in [None, "", [], {}, (), "undefined"]]
     r = int(input("Enter radius: "))
     s = math.radians(int(input("Enter start angle: ")))
     e = math.radians(int(input("Enter end angle: ")))
     a1 = acad.model.AddArc(APoint(c[0], c[1]), r, s, e)
+    if input("Reverse arc?(y/n)")=="y":
+        a1.ReverseOrientation()
 while True:
-    command = input("Enter ll, lr, c, a, p: ")
+    command = input("Enter ll, lr, c, a, p, e: ").strip().lower()
     if (command == "ll"):
         lineL()
+        acad.app.ZoomExtents()
     elif (command == "lr"):
         lineR()
+        acad.app.ZoomExtents()
     elif (command == "c"):
         circle()
+        acad.app.ZoomExtents()
     elif (command == "a"):
         arc()
+        acad.app.ZoomExtents()
     elif (command == "p"):
         polyline()
-    a = input("Type \'e\' to end or Type \'c\' to continue: ")
-    if a == "e":
+        acad.app.ZoomExtents()
+    elif command=="e":
         break
+    else:
+        continue
